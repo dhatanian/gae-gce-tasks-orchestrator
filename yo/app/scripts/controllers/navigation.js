8f53703d85136eaa127f49109cc515efb491ce33
David Hatanian <david.hatanian@revevol.eu>
2014-04-11 09:08:23 +0200

    fixed issue with popup blocking, now let's deal with the cron job

--- yo/app/scripts/controllers/navigation.js
+++ yo/app/scripts/controllers/navigation.js
@@ -1,15 +1,26 @@
 'use strict';
 
 angular.module('yoApp')
-    .controller('NavCtrl', function ($scope, Executionsservice,$location) {
+    .controller('NavCtrl', function ($scope, Executionsservice, $location, $modal) {
         $scope.username = "Checking access rights...";
+        $scope.login = Executionsservice.openLoginWindow;
+
         Executionsservice.promise.then(function () {
             Executionsservice.checkLogin().then(function (user) {
                 $scope.username = user.email;
-                if(!user.admin){
+                if (!user.admin) {
                     //window.uri = "#/denied"
                     $location.path("/denied");
                 }
             });
+        });
+
+        Executionsservice.needsLogin.then(function () {
+            $modal.open(
+                {
+                    template: '<div class="modal-body"><p>Click here to be authenticated</p></div><div class="modal-footer"> <a class="btn btn-primary" ng-click="login();$close()">Log in</a></div>',
+                    scope: $scope
+                }
+            );
         });
     });
--- yo/app/scripts/services/executionsService.js
+++ yo/app/scripts/services/executionsService.js
@@ -5,44 +5,8 @@ var user = 'Anonymous user';
 var userPicUrl = '/questionmark.png';
 
 function onGoogleLibraryLoaded() {
-    var orchestratorLoaded = false;
-    var oauthLoaded = false;
-    var userAuthenticated = false;
-
-    var globalCallback = function () {
-        if (orchestratorLoaded && oauthLoaded && userAuthenticated) {
-            googleLibraryLoaded = true;
-            googleLibraryCallback();
-        }
-    }
-
-    var signin = function (immediate, callback) {
-        gapi.auth.authorize({client_id: '102862643449-geb89aoann7dj6tsha4mtkhvos5mk01b.apps.googleusercontent.com', scope: 'https://www.googleapis.com/auth/userinfo.email',
-            immediate: immediate}, callback);
-    }
-
-    var authorizationCallback = function (data) {
-        if (data == null) {
-            signin(false, authorizationCallback);
-        } else {
-            userAuthenticated = true;
-            globalCallback();
-        }
-    }
-
-    signin(true, authorizationCallback);
-
-    var ROOT = '/_ah/api';
-    gapi.client.load('orchestrator', 'v1', function () {
-        orchestratorLoaded = true;
-        globalCallback();
-    }, ROOT);
-
-    gapi.client.load('oauth2', 'v2', function () {
-        console.log('oauth2 loaded')
-        oauthLoaded = true;
-        globalCallback();
-    });
+    googleLibraryLoaded = true;
+    googleLibraryCallback();
 }
 
 var googleLibraryCallback = function () {
@@ -52,15 +16,58 @@ angular.module('yoApp')
     .service('Executionsservice', function Executionsservice($q) {
         // AngularJS will instantiate a singleton by calling 'new' on this function
         var deferred = $q.defer();
-        if (googleLibraryLoaded) {
-            deferred.resolve();
-        } else {
-            googleLibraryCallback = function () {
+        var needsLoginDeferred = $q.defer();
+
+        var signin = function (immediate, callback) {
+            gapi.auth.authorize({client_id: '102862643449-geb89aoann7dj6tsha4mtkhvos5mk01b.apps.googleusercontent.com', scope: 'https://www.googleapis.com/auth/userinfo.email',
+                immediate: immediate}, callback);
+        }
+
+        var orchestratorLoaded = false;
+        var oauthLoaded = false;
+        var userAuthenticated = false;
+
+        var globalCallback = function () {
+            if (orchestratorLoaded && oauthLoaded && userAuthenticated) {
                 deferred.resolve();
             }
         }
 
+        var authorizationCallback = function (data) {
+            if (data == null) {
+                needsLoginDeferred.resolve();
+            } else {
+                userAuthenticated = true;
+                globalCallback();
+            }
+        }
+
+        var afterGoogleLibraryLoaded = function () {
+            signin(true, authorizationCallback);
+
+            var ROOT = '/_ah/api';
+            gapi.client.load('orchestrator', 'v1', function () {
+                orchestratorLoaded = true;
+                globalCallback();
+            }, ROOT);
+
+            gapi.client.load('oauth2', 'v2', function () {
+                oauthLoaded = true;
+                globalCallback();
+            });
+        }
+
+        if (googleLibraryLoaded) {
+            afterGoogleLibraryLoaded();
+        } else {
+            googleLibraryCallback = afterGoogleLibraryLoaded;
+        }
+
         return {
+            openLoginWindow: function () {
+                signin(false, authorizationCallback);
+            },
+            needsLogin: needsLoginDeferred.promise,
             startExecution: function (execution) {
                 var deferred = $q.defer();
                 gapi.client.orchestrator.executions.start(execution).execute(function (resp) {
